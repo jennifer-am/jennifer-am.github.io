7d53340c1dcff9909f067a62146852e7b40d8c2f
Jennifer AMETEPE <kouessiba-jennifer-caridad.ametepe@epitech.eu>
2022-02-10 15:35:39 +0000

    commit

---
+++ info.js
@@ -0,0 +1,7 @@
+// let github_link = "https://jennifer-am.github.io/data.json";
+
+// $.getJSON(github_link, function(data) {
+//      //data is the JSON string
+     
+//      console.log(data);
+// });
